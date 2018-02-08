bf1d413c89930a9e7af9f99ee8a95c2ff215adda
295_92 <295_92@naver.com>
2018-02-08 14:54:30 +0900

    no message

--- 02 JSP Servlet/01 보충교재/Web보충교재11 JDBC 연동 Dept(Model 1)_20180206/src폴더에 복사하세요/jdbcex/dept/Main1.java
+++
@@ -1,9 +0,0 @@
-package jdbcex.dept;
-public class Main1 {
-	public static void main(String[] args) {
-		DeptDAO deptDao = new DeptDAO(); 
-        //10번 부서 정보 select 
-		DeptVO vo1 = deptDao.getDept(10); 
-		System.out.println(vo1.getDeptNo()+":"+vo1.getDname()+":"+ vo1.getLoc()); 
-	}
-}
--- 02 JSP Servlet/02 lab/답/lab13 답/WebContent 폴더에 복사하세요/_lab13/logout.jsp
+++
@@ -1,4 +0,0 @@
-<%@ page contentType="text/html; charset=UTF-8"%>
-<!DOCTYPE html><html><body>
-<% session.invalidate();  %> 로그 아웃 되었습니다.
-</body></html>
